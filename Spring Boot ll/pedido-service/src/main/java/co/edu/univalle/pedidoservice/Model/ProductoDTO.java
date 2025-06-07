427fd0f1857097da59f8b7d044787e33d3535a27
Victor Alzate <victoralzate61@gmail.com>
2025-06-07 14:40:12 -0500

    feat: Creando Servicio REST en Spring Boot

---
+++ Spring Boot ll/pedido-service/src/main/java/co/edu/univalle/pedidoservice/Model/ProductoDTO.java
@@ -0,0 +1,17 @@
+package co.edu.univalle.pedidoservice.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductoDTO {
+    private Long id;
+    private String nombre;
+    private Double precio;
+
+    @Override
+    public String toString() {
+        return "ProductoDTO{" + "id=" + id + ", nombre='" + nombre + '\'' + ", precio=" + precio + '}';
+    }
+}
